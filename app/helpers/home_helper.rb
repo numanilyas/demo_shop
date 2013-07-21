d4dec9c0b633e7f62c48af97b079053c7321a0c4
Numan Ilyas <numan.ilyas@gmail.com>
2013-07-21 14:19:06 -0400

    m

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,13 @@ module HomeHelper
   def get_product_id(sample_id)
     product_id = ProductSample.find(sample_id).product.slug
   end
+  
+  def trim_meta_description(description)
+    if description.length > 160
+      desc = description[0..157] + "..."
+    else
+      description
+    end
+    
+  end
 end
