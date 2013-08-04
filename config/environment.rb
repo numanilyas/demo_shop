e2618364dc8da411e54e662fe7cd58501d9e3878
Numan Ilyas <numan.ilyas@gmail.com>
2013-08-04 10:29:35 -0400

    m

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,8 @@ Embroideryshop::Application.configure do
   
   config.action_mailer.smtp_settings = {
     :address => ENV['SMTP'],
-    :port => ENV['SMTP_PORT'],     
+    :port => ENV['SMTP_PORT'],
+    #:domain => "www.embroideryshop.com", 
     :authentication => ENV['SMTP_AUTHENTICATION'],
     :user_name => ENV['SMTP_USER'],
     :password =>  ENV['SMTP_PASSWORD'],
